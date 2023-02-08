4c1f50668ea0e10b3edf7276f56bb9e032caed16
John Slowik <john@jslowik.com>
2023-02-07 18:18:05 -0600

    Writing to s3 bucket

--- functions/collectNextRocketLaunches/main.go
+++ functions/collectNextRocketLaunches/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"functions/models"
-	runtime "github.com/aws/aws-lambda-go/lambda"
 	"io"
 	"net/http"
+	"os"
+	"strings"
+
+	runtime "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+type BucketBasics struct {
+	S3Client *s3.Client
+}
 
 func getNext5Launches() models.UpcomingRocketLaunchesApiResponse {
 	resp, err := http.Get("https://fdo.rocketlaunch.live/json/launches/next/5")
@@ -31,8 +43,41 @@ func getNext5Launches() models.UpcomingRocketLaunchesApiResponse {
 
 	return rockets
 }
+
+func storeNextLaunchesInS3(rockets models.UpcomingRocketLaunchesApiResponse) {
+	cfg, _ := config.LoadDefaultConfig(context.TODO(), func(o *config.LoadOptions) error {
+		o.Region = "us-east-1"
+		return nil
+	})
+
+	basics := BucketBasics{
+		S3Client: s3.NewFromConfig(cfg),
+	}
+
+	rocketJson, jsonMarshalErr := json.Marshal(rockets)
+
+	if jsonMarshalErr != nil {
+		fmt.Println("Failed to marshal json", jsonMarshalErr)
+	}
+
+	reader := strings.NewReader(string(rocketJson))
+
+	uploader := manager.NewUploader(basics.S3Client)
+	_, uploadErr := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("DATA_BUCKET")),
+		Key:    aws.String(os.Getenv("BUCKET_KEY")),
+		Body:   reader,
+	})
+
+	if uploadErr != nil {
+		fmt.Println("Failed to upload file to S3", uploadErr)
+	}
+
+}
+
 func handler() {
 	rockets := getNext5Launches()
+	storeNextLaunchesInS3(rockets)
 	fmt.Println(rockets)
 }
 func main() {
