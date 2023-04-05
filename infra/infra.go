bd3910794c3657557b6c139c69d8e21532a0d95c
John Slowik <john@jslowik.com>
2023-04-05 16:27:16 -0500

    REM function

--- infra/infra.go
+++ infra/infra.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awscertificatemanager"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsevents"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awseventstargets"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
@@ -67,30 +65,6 @@ func SpaceCloudInfraStack(scope constructs.Construct, id string, props *InfraSta
 		},
 	})
 
-	// Read people in space function.
-	readPeopleFunction := awslambda.NewFunction(stack, jsii.String("ReadPeople"), &awslambda.FunctionProps{
-		FunctionName: jsii.String(*stack.StackName() + "-ReadPeople"),
-		Runtime:      awslambda.Runtime_GO_1_X(),
-		MemorySize:   jsii.Number(128),
-		Timeout:      awscdk.Duration_Seconds(jsii.Number(60)),
-		Code:         awslambda.AssetCode_FromAsset(jsii.String("../out/."), nil),
-		Handler:      jsii.String("readPeopleInSpace"),
-		Architecture: awslambda.Architecture_X86_64(),
-		Role:         s3LambdaRole,
-		LogRetention: awslogs.RetentionDays_ONE_WEEK,
-		CurrentVersionOptions: &awslambda.VersionOptions{
-			RemovalPolicy: awscdk.RemovalPolicy_DESTROY,
-		},
-		Environment: &map[string]*string{
-			"DATA_BUCKET": jsii.String(*bucket.BucketName()),
-			"BUCKET_KEY":  jsii.String("people_in_space.json"),
-		},
-	})
-
-	// Read people in space rust function
-	readPeopleRustFunction := awslambda.Function_FromFunctionArn(stack, jsii.String("ReadPeopleRust"),
-		jsii.String("arn:aws:lambda:us-east-1:939984321277:function:read-people-in-space-rust"))
-
 	// Retrieve and store launch lambda function.
 	collectLaunchFunction := awslambda.NewFunction(stack, jsii.String("GetLaunches"), &awslambda.FunctionProps{
 		FunctionName: jsii.String(*stack.StackName() + "-GetLaunches"),
@@ -115,9 +89,6 @@ func SpaceCloudInfraStack(scope constructs.Construct, id string, props *InfraSta
 	// Read and write
 	bucket.GrantReadWrite(collectPeopleFunction, nil)
 	bucket.GrantReadWrite(collectLaunchFunction, nil)
-
-	// Read only
-	bucket.GrantRead(readPeopleFunction, nil)
 
 	// "Daily" event - every 2 hours
 	everyTwoHoursEventRule := awsevents.NewRule(
@@ -150,82 +121,6 @@ func SpaceCloudInfraStack(scope constructs.Construct, id string, props *InfraSta
 	// Add targets to event rule(s)
 	everyTwoHoursEventRule.AddTarget(awseventstargets.NewLambdaFunction(collectPeopleFunction, nil))
 	twiceDailyEventRule.AddTarget(awseventstargets.NewLambdaFunction(collectLaunchFunction, nil))
-
-	// Create API Gateway
-	restApiProd := awsapigateway.NewRestApi(
-		stack,
-		jsii.String("space_cloud_api"),
-		&awsapigateway.RestApiProps{
-			RestApiName:        jsii.String("Space Cloud API"),
-			RetainDeployments:  jsii.Bool(false),
-			EndpointExportName: jsii.String("SpaceCloudApiEndpoint"),
-			Deploy:             jsii.Bool(true),
-			EndpointConfiguration: &awsapigateway.EndpointConfiguration{
-				Types: &[]awsapigateway.EndpointType{
-					awsapigateway.EndpointType_REGIONAL,
-				},
-			},
-			DeployOptions: &awsapigateway.StageOptions{
-				StageName:            jsii.String("prod"),
-				CacheClusterEnabled:  jsii.Bool(false),
-				ThrottlingBurstLimit: jsii.Number(100),
-				ThrottlingRateLimit:  jsii.Number(1000),
-			},
-			DomainName: &awsapigateway.DomainNameOptions{
-				DomainName: jsii.String("api.spacebits.net"),
-				Certificate: awscertificatemanager.Certificate_FromCertificateArn(
-					stack,
-					jsii.String("space_cloud_api_cert"),
-					jsii.String("arn:aws:acm:us-east-1:939984321277:certificate/32c01289-82c7-4d30-885a-d5cd3aab4a93"),
-				),
-			},
-			DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
-				AllowOrigins: awsapigateway.Cors_ALL_ORIGINS(),
-				AllowMethods: awsapigateway.Cors_ALL_METHODS(),
-				AllowHeaders: awsapigateway.Cors_DEFAULT_HEADERS(),
-			},
-		},
-	)
-
-	// Read people endpoint
-	readPeopleResource := restApiProd.Root().AddResource(jsii.String("people"), nil)
-	readPeopleResource.AddMethod(jsii.String("GET"), awsapigateway.NewLambdaIntegration(readPeopleFunction,
-		&awsapigateway.LambdaIntegrationOptions{}),
-		&awsapigateway.MethodOptions{
-			ApiKeyRequired: jsii.Bool(true),
-		})
-
-	readPeopleRustResource := restApiProd.Root().AddResource(jsii.String("peoplerust"), nil)
-	readPeopleRustResource.AddMethod(jsii.String("GET"), awsapigateway.NewLambdaIntegration(readPeopleRustFunction,
-		&awsapigateway.LambdaIntegrationOptions{}),
-		&awsapigateway.MethodOptions{
-			ApiKeyRequired: jsii.Bool(true),
-		})
-
-	// Set up usage plan for API
-	usagePlan := restApiProd.AddUsagePlan(jsii.String("UsagePlan"), &awsapigateway.UsagePlanProps{
-		Name: jsii.String(*stack.StackName() + "-UsagePlan"),
-		Throttle: &awsapigateway.ThrottleSettings{
-			BurstLimit: jsii.Number(10),
-			RateLimit:  jsii.Number(100),
-		},
-		Quota: &awsapigateway.QuotaSettings{
-			Limit:  jsii.Number(10000),
-			Offset: jsii.Number(0),
-			Period: awsapigateway.Period_DAY,
-		},
-		ApiStages: &[]*awsapigateway.UsagePlanPerApiStage{
-			{
-				Api:      restApiProd,
-				Stage:    restApiProd.DeploymentStage(),
-				Throttle: &[]*awsapigateway.ThrottlingPerMethod{},
-			},
-		},
-	})
-
-	// Create ApiKey and associate it with UsagePlan.
-	apiKey := restApiProd.AddApiKey(jsii.String("ApiKey"), &awsapigateway.ApiKeyOptions{})
-	usagePlan.AddApiKey(apiKey, &awsapigateway.AddApiKeyOptions{})
 
 	return stack
 }
